13e6a2ad659e8d81233f79bc6798a66fa7daa9f9
Lukz <lucaseduarte6@gmail.com>
2026-05-22 20:33:49 -0300

    MVC FOIIII

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const authController = require('../controllers/authController');
+const authController = require('../controllers/authController.js');
 
 router.get('/', authController.showHome);
 router.get('/termos', authController.showTermos);
